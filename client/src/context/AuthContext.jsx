b6dcf11d7d367971d70f53b12ab8ea85578e1927
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2025-08-18 18:27:48 +0530

    chnage redirects

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -8,7 +8,6 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-  const navigate = useNavigate();
 
   const checkSession = async () => {
     try {
--- client/src/routes/HomeRedirect.jsx
+++ client/src/routes/HomeRedirect.jsx
@@ -1,28 +1,31 @@
 import React, { useContext, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
+import LoadBalls from "../components/LoadBalls";
 
 const HomeRedirect = () => {
   const { user, loading } = useContext(AuthContext);
 
-  if (!loading) {
-    if (!user) {
+  if (loading) {
+    return <LoadBalls />;
+  }
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  // Logged in - redirect based on role
+
+  switch (user.role) {
+    case "admin":
+      return <Navigate to="/admin/dashboard" replace />;
+    case "store_owner":
+      return <Navigate to="/store/dashboard" replace />;
+    case "normal_user":
+      return <Navigate to="/user/dashboard" replace />;
+    default:
+      // Fallback: send to login or somewhere else
       return <Navigate to="/login" replace />;
-    }
-
-    // Logged in - redirect based on role
-
-    switch (user.role) {
-      case "admin":
-        return <Navigate to="/admin/dashboard" replace />;
-      case "store_owner":
-        return <Navigate to="/store/dashboard" replace />;
-      case "normal_user":
-        return <Navigate to="/user/dashboard" replace />;
-      default:
-        // Fallback: send to login or somewhere else
-        return <Navigate to="/login" replace />;
-    }
   }
 };
 
